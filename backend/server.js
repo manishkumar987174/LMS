4dfe426dd0f08a36654456c68f00086264988866
manishkumar987174 <manishkumar987174@gmail.com>
2026-03-25 20:21:20 +0530

    error solved

--- backend/server.js
+++ backend/server.js
@@ -14,21 +14,20 @@ app.use("/api/institute", require("./routes/instituteRoutes"));
 app.use("/api/books", require("./routes/bookRoutes"));
 app.use("/api/course", require("./routes/courseRoutes"));
 app.use("/api/enroll", require("./routes/enrollRoutes"));
-app.use("/api/issue", require("./routes/issueRoutes")); // 👈 THIS
+app.use("/api/issue", require("./routes/issueRoutes")); 
 app.use("/api/certificate", require("./routes/certificateRoutes"));
 app.use("/api/test", require("./routes/testRoutes"));
 app.use("/api/reports", require("./routes/reportRoutes"));
 
 // MongoDB
 mongoose
-  .connect("mongodb+srv://backend:abcd12345@backend.sorrlwl.mongodb.net/lms");
+  .connect("mongodb+srv://backend:abcd12345@backend.sorrlwl.mongodb.net/lms")
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log("Mongo Error:", err));
 
-// // Health check
-// app.get("/", (req, res) => {
-//   res.send("Library SaaS Backend Running ");
-// });
+app.get("/", (req, res) => {
+  res.send("Library Backend Running ");
+});
 
 // Start server
 app.listen(5000, () => {
